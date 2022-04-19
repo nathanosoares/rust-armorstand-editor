c6665acc8bc25c71e7c00277577fddebe5ab5e4f
Nathan Soares <eu@nathan.com.br>
2022-04-19 12:25:22 -0300

    development

--- src/armor_stand.rs
+++ src/armor_stand.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ecs::system::EntityCommands, prelude::*, render::texture::TranscodeFormat};
 
 use crate::controls::Controllable;
 
@@ -6,9 +6,36 @@ pub struct ArmorStandDummyPlugin;
 
 impl Plugin for ArmorStandDummyPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_armor_stand)
-            .add_system(rotator_system);
+        app.insert_resource(ArmorStandRequester::default())
+            .add_startup_system(spawn_armor_stand)
+            .add_system(rotator_system)
+            .add_system(armor_stand_requester_system);
     }
+}
+
+#[derive(Default)]
+pub struct ArmorStandRequester {
+    request: bool,
+}
+
+impl ArmorStandRequester {
+    pub fn request_armor_stand(&mut self) {
+        self.request = true;
+    }
+
+    fn requeted(&mut self) {
+        self.request = false;
+    }
+}
+
+#[derive(Component, Default)]
+pub struct ArmorStand;
+
+#[derive(Bundle, Default)]
+pub struct ArmorStandBundle {
+    armor_stand: ArmorStand,
+    transform: Transform,
+    global_tranform: GlobalTransform,
 }
 
 fn spawn_armor_stand(
@@ -16,94 +43,100 @@ fn spawn_armor_stand(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    create_armor_stand_part(
-        &mut commands,
-        &mut meshes,
-        &mut materials,
-        "Head".to_owned(),
-        Vec3::new(0.0, 22.0, 0.0),
-        vec![BoxProps {
-            position: Vec3::new(0.0, 3.5, 0.0),
-            size: Vec3::new(2.0, 7.0, 2.0),
-        }],
-    );
+    commands
+        .spawn()
+        .insert_bundle(ArmorStandBundle::default())
+        .with_children(|mut child_builder| {
+            create_armor_stand_part(
+                &mut child_builder,
+                &mut meshes,
+                &mut materials,
+                "Head".to_owned(),
+                Vec3::new(0.0, 22.0, 0.0),
+                vec![BoxProps {
+                    position: Vec3::new(0.0, 3.5, 0.0),
+                    size: Vec3::new(2.0, 7.0, 2.0),
+                }],
+            );
 
-    create_armor_stand_part(
-        &mut commands,
-        &mut meshes,
-        &mut materials,
-        "Body".to_owned(),
-        Vec3::new(0.0, 23.0, 0.0),
-        vec![
-            BoxProps {
-                position: Vec3::new(0.0, -11.0, 0.0),
-                size: Vec3::new(8.0, 2.0, 2.0),
-            },
-            BoxProps {
-                position: Vec3::new(-2.0, -6.5, 0.0),
-                size: Vec3::new(2.0, 7.0, 2.0),
-            },
-            BoxProps {
-                position: Vec3::new(2.0, -6.5, 0.0),
-                size: Vec3::new(2.0, 7.0, 2.0),
-            },
-            BoxProps {
-                position: Vec3::new(0.0, -1.5, 0.0),
-                size: Vec3::new(12.0, 3.0, 3.0),
-            },
-        ],
-    );
+            create_armor_stand_part(
+                &mut child_builder,
+                &mut meshes,
+                &mut materials,
+                "Body".to_owned(),
+                Vec3::new(0.0, 23.0, 0.0),
+                vec![
+                    BoxProps {
+                        position: Vec3::new(0.0, -11.0, 0.0),
+                        size: Vec3::new(8.0, 2.0, 2.0),
+                    },
+                    BoxProps {
+                        position: Vec3::new(-2.0, -6.5, 0.0),
+                        size: Vec3::new(2.0, 7.0, 2.0),
+                    },
+                    BoxProps {
+                        position: Vec3::new(2.0, -6.5, 0.0),
+                        size: Vec3::new(2.0, 7.0, 2.0),
+                    },
+                    BoxProps {
+                        position: Vec3::new(0.0, -1.5, 0.0),
+                        size: Vec3::new(12.0, 3.0, 3.0),
+                    },
+                ],
+            );
 
-    create_armor_stand_part(
-        &mut commands,
-        &mut meshes,
-        &mut materials,
-        "Left Arm".to_owned(),
-        Vec3::new(6.0, 21.0, 0.0),
-        vec![BoxProps {
-            position: Vec3::new(0.0, -4.0, 0.0),
-            size: Vec3::new(2.0, 12.0, 2.0),
-        }],
-    );
+            create_armor_stand_part(
+                &mut child_builder,
+                &mut meshes,
+                &mut materials,
+                "Left Arm".to_owned(),
+                Vec3::new(6.0, 21.0, 0.0),
+                vec![BoxProps {
+                    position: Vec3::new(0.0, -4.0, 0.0),
+                    size: Vec3::new(2.0, 12.0, 2.0),
+                }],
+            );
 
-    create_armor_stand_part(
-        &mut commands,
-        &mut meshes,
-        &mut materials,
-        "Right Arm".to_owned(),
-        Vec3::new(-6.0, 21.0, 0.0),
-        vec![BoxProps {
-            position: Vec3::new(0.0, -4.0, 0.0),
-            size: Vec3::new(2.0, 12.0, 2.0),
-        }],
-    );
+            create_armor_stand_part(
+                &mut child_builder,
+                &mut meshes,
+                &mut materials,
+                "Right Arm".to_owned(),
+                Vec3::new(-6.0, 21.0, 0.0),
+                vec![BoxProps {
+                    position: Vec3::new(0.0, -4.0, 0.0),
+                    size: Vec3::new(2.0, 12.0, 2.0),
+                }],
+            );
 
-    create_armor_stand_part(
-        &mut commands,
-        &mut meshes,
-        &mut materials,
-        "Left Leg".to_owned(),
-        Vec3::new(2.0, 11.0, 0.0),
-        vec![BoxProps {
-            position: Vec3::new(0.0, -5.5, 0.0),
-            size: Vec3::new(2.0, 11.0, 2.0),
-        }],
-    );
-    create_armor_stand_part(
-        &mut commands,
-        &mut meshes,
-        &mut materials,
-        "Right Leg".to_owned(),
-        Vec3::new(-2.0, 11.0, 0.0),
-        vec![BoxProps {
-            position: Vec3::new(0.0, -5.5, 0.0),
-            size: Vec3::new(2.0, 11.0, 2.0),
-        }],
-    );
+            create_armor_stand_part(
+                &mut child_builder,
+                &mut meshes,
+                &mut materials,
+                "Left Leg".to_owned(),
+                Vec3::new(2.0, 11.0, 0.0),
+                vec![BoxProps {
+                    position: Vec3::new(0.0, -5.5, 0.0),
+                    size: Vec3::new(2.0, 11.0, 2.0),
+                }],
+            );
+
+            create_armor_stand_part(
+                &mut child_builder,
+                &mut meshes,
+                &mut materials,
+                "Right Leg".to_owned(),
+                Vec3::new(-2.0, 11.0, 0.0),
+                vec![BoxProps {
+                    position: Vec3::new(0.0, -5.5, 0.0),
+                    size: Vec3::new(2.0, 11.0, 2.0),
+                }],
+            );
+        });
 }
 
 #[derive(Bundle, Default)]
-struct PartBundle {
+struct ArmorStandPartBundle {
     transform: Transform,
     visibility: Visibility,
     global_tranform: GlobalTransform,
@@ -118,15 +151,15 @@ struct BoxProps {
 }
 
 fn create_armor_stand_part(
-    commands: &mut Commands,
+    child_builder: &mut ChildBuilder,
     meshes: &mut ResMut<Assets<Mesh>>,
     materials: &mut ResMut<Assets<StandardMaterial>>,
     label: String,
     position: Vec3,
     boxes: Vec<BoxProps>,
 ) {
-    commands
-        .spawn_bundle(PartBundle {
+    child_builder
+        .spawn_bundle(ArmorStandPartBundle {
             transform: Transform::from_xyz(
                 convert_to_units(position.x),
                 convert_to_units(position.y),
@@ -162,6 +195,18 @@ fn convert_to_units(value: f32) -> f32 {
 #[derive(Component, Default, Debug)]
 pub struct Rotator {
     pub euler: Vec3,
+}
+
+fn armor_stand_requester_system(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut requester: ResMut<ArmorStandRequester>,
+) {
+    if requester.request {
+        spawn_armor_stand(commands, meshes, materials);
+        requester.requeted();
+    }
 }
 
 pub fn rotator_system(mut query: Query<(&mut Transform, &Rotator)>) {
--- src/controls.rs
+++ src/controls.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext, EguiPlugin};
 
-use crate::armor_stand::Rotator;
+use crate::armor_stand::{ArmorStand, ArmorStandBundle, ArmorStandRequester, Rotator};
 pub struct ControlsPlugin;
 
 impl Plugin for ControlsPlugin {
@@ -17,18 +17,50 @@ pub struct Controllable {
 
 fn controllable_system(
     mut egui_context: ResMut<EguiContext>,
-    mut query: Query<(&mut Rotator, &Controllable)>,
+    mut stand_query: Query<(&ArmorStand, &mut Transform)>,
+    mut parts_query: Query<(&mut Rotator, &Controllable)>,
+    mut requester: ResMut<ArmorStandRequester>,
 ) {
     egui::Window::new("Armor Stand Controls")
         .default_width(500.0)
         .resizable(true)
         .show(egui_context.ctx_mut(), |ui| {
             ui.add_enabled_ui(true, |ui| {
+                if ui.button("New armor stand").clicked() {
+                    requester.request_armor_stand();
+                }
+
+                for (_, mut transform) in stand_query.iter_mut() {
+                    ui.horizontal(|ui| {
+                        ui.add(
+                            egui::DragValue::new(&mut transform.translation.x)
+                                .speed(0.1)
+                                .fixed_decimals(2),
+                        )
+                        .on_hover_text("Move X");
+
+                        ui.add(
+                            egui::DragValue::new(&mut transform.translation.y)
+                                .speed(0.1)
+                                .fixed_decimals(2),
+                        )
+                        .on_hover_text("Move Y");
+                        ui.add(
+                            egui::DragValue::new(&mut transform.translation.z)
+                                .speed(0.1)
+                                .fixed_decimals(2),
+                        )
+                        .on_hover_text("Move Z");
+                    });
+                }
+
+                ui.separator();
+
                 egui::Grid::new("my_grid")
                     .num_columns(2)
                     .spacing([10.0, 4.0])
                     .show(ui, |ui| {
-                        for (mut rotator, controllable) in query.iter_mut() {
+                        for (mut rotator, controllable) in parts_query.iter_mut() {
                             ui.label(controllable.label.clone());
 
                             ui.add(
